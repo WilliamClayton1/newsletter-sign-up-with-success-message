c02751be72a0bb7e528b201af0f3af862c769e4d
William Clayton <william.d.clayton1@gmail.com>
2023-10-11 09:41:34 -0500

    -wc

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 submitButton.addEventListener('click', (e) => {
 
+    const invalidText = document.querySelector('#invalidEmail');
+
     e.preventDefault();
 
     if(input.value.match(mailFormat))
@@ -13,7 +15,10 @@ submitButton.addEventListener('click', (e) => {
         console.log(true);//valid
     }
     else {
-        console.log(false);//invalid
+        invalidText.style.display = 'flex';
+        input.style.color = '#FF6155'
+        input.style.borderColor = '#FF6155';
+        input.style.backgroundColor = 'rgba(255, 97, 85, 0.15)';
     }
 
 });
